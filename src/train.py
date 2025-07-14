0654536034e0d8bf8a4bb115abd6b3416c934053
Srijan Srivastava <QCoreNest@gmail.com>
2025-07-14 23:24:35 +0530

    refactor

--- src/train.py
+++ src/train.py
@@ -1,22 +1,19 @@
-from utils import calc_total_time, kprint
 from model import Config, Palm, sample
 from colorama import Style, Fore, init
 from encoder import Encoder
+from pathlib import Path
 from contextlib import nullcontext
 from rich.progress import track
-from pathlib import Path
 import torch, random, numpy, time, math, os
-import torch.amp, json, sys
+import torch.amp, json, regex, sys
 torch._inductor.config.coordinate_descent_tuning = True
 torch._dynamo.config.compiled_autograd = True
 
-init(autoreset=True)
-
 # load config
+init(autoreset=True)
 CONFIG_PATH = sys.argv[1] if len(sys.argv) > 1 else "script/config.json"
 with open(CONFIG_PATH, "r", encoding="utf-8") as f:
 	CONFIG = json.load(f)
-model_log_path = os.path.join(Path(CONFIG["save_path"]).parent.name, "out.txt")
 
 # set device
 device = ("cuda" if torch.cuda.is_available() else "cpu") if CONFIG["device"] == "auto" else CONFIG["device"]
@@ -26,24 +23,45 @@ if CONFIG["seed"] != "auto":
 	numpy.random.seed(CONFIG["seed"])
 	random.seed(CONFIG["seed"])
 
-def init_model(checkpoint=None):
-	global model_log_path
-	# if model is init from scratch then create an empty model log file.
-	if checkpoint is None:
-		with open(model_log_path, "w", encoding="utf-8") as f:
-			f.write("")
+# save the text in a text file
+ansi_escape = regex.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+def print0(*text, println=True):
+    print(*text) if println else None
 
+    # save cleaned text to the file
+    with open(os.path.join(Path(CONFIG["save_path"]).parent.name, "out.txt"), "a", encoding="utf-8") as f:
+        f.write(" ".join(tuple(ansi_escape.sub('', part) for part in text)) + "\n")
+
+def calc_total_time(seconds):
+    # separate the integer part (for hours, minutes, and seconds) from the fractional part (for milliseconds)
+    sec_int, millis = divmod(seconds, 1)
+    millis = int(millis * 1000) # convert the fractional part to milliseconds
+
+    min, sec = divmod(int(sec_int), 60)
+    hour, min = divmod(min, 60)
+    hours, minutes, seconds = int(hour), int(min), int(sec)
+
+    t = [
+        f"{hours} hour" + ("s" if hours > 1 else "") if hours > 0 else None,
+        f"{minutes} minute" + ("s" if minutes > 1 else "") if minutes > 0 else None,
+        f"{seconds} second" + ("s" if seconds > 1 else "") if seconds > 0 else None,
+        f"{millis} ms" if millis > 0 else None
+    ]
+    t = list(filter(None, t))
+
+    return ", ".join(t) if t else "0 seconds"
+
+def init_model(checkpoint=None):
 	# print the device
-	kprint(f"```config.json\n{json.dumps(CONFIG, indent=4)}\n```", filename=model_log_path, println=False)
-	kprint("Training on", f"{Fore.YELLOW}{Style.BRIGHT}{device}", f"{Fore.WHITE}{Style.BRIGHT}({torch.initial_seed()})", filename=model_log_path)
+	print0(f"```config.json\n{json.dumps(CONFIG, indent=4)}\n```", println=False)
+	print0("Training on", f"{Fore.YELLOW}{Style.BRIGHT}{device}", f"{Fore.WHITE}{Style.BRIGHT}({torch.initial_seed()})")
 
 	# load stats
 	stats = checkpoint["stats"] if checkpoint is not None and "stats" in checkpoint.keys() else {
-		"iter_num": 0,
+		"steps": 0,
 		"train": [],
 		"eval": [],
 		"val": [],
-		"mfu": [],
 		"lr": []
 	}
 
@@ -63,11 +81,9 @@ def init_model(checkpoint=None):
 	if checkpoint is not None:
 		state_dict = checkpoint["model"]
 		unwanted_prefix = '_orig_mod.'
-
 		for k, v in list(state_dict.items()):
 			if k.startswith(unwanted_prefix):
 				state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-
 		# load the state dict
 		model.load_state_dict(state_dict)
 	model.to(device)
@@ -75,12 +91,6 @@ def init_model(checkpoint=None):
 	# optimizer
 	optimizer = model.configure_optimizers(CONFIG["weight_decay"], CONFIG["learning_rate"], CONFIG["device"])
 	optimizer.load_state_dict(checkpoint["optimizer"]) if checkpoint is not None else None
-
-	# crop down the model block size if desired, using model surgery
-	if CONFIG["block_size"] < hyperparams["block_size"]:
-		model.crop_block_size(CONFIG["block_size"])
-		hyperparams["block_size"] = CONFIG["block_size"] # so that the checkpoint will have the right value
-
 	return model, optimizer, hyperparams, stats
 
 # learning rate decay scheduler (cosine with warmup)
@@ -110,7 +120,7 @@ def estimate_loss(eval_iters, model, get_batch):
 		for k in track(range(eval_iters), description=f"{Fore.WHITE}{Style.BRIGHT}calc {Fore.WHITE}{Style.DIM}{split} loss{Style.RESET_ALL}"):
 			X, Y = get_batch(split)
 			with ctx:
-				logits, loss = model(X, Y)
+				_, loss = model(X, Y)
 
 			losses[k] = loss.item()
 		out[split] = losses.mean()
@@ -176,7 +186,7 @@ class dataloader:
 
 # init model and optimizer
 model, optimizer, hyperparams, stats = init_model(torch.load(CONFIG["init_from"][11:]) if CONFIG["init_from"].startswith("pretrained,") else None)
-kprint("using RoPE:", f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}True", filename=model_log_path)
+print0("using RoPE:", f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}True")
 
 # "float32", "bfloat16", or "float16", the latter will auto implement a GradScaler
 dtype = "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "float16"
@@ -196,23 +206,23 @@ get_batch = lambda x: train_data_loader.next_batch() if x == "train" else val_da
 # print the number of tokens
 num_train_toks = train_data_loader.get_tok_count()
 num_val_toks = val_data_loader.get_tok_count()
-kprint(f"{Fore.WHITE}{Style.BRIGHT}{((num_train_toks + num_val_toks)/1e6)}M", "total tokens", filename=model_log_path)
-kprint(
+print0(f"{Fore.WHITE}{Style.BRIGHT}{((num_train_toks + num_val_toks)/1e6)}M", "total tokens")
+print0(
 	f"{Fore.WHITE}{Style.BRIGHT}{(num_train_toks/1e6)}M", "train tokens,", f"{Fore.WHITE}{Style.BRIGHT}{(num_val_toks/1e6)}M", "val tokens",
-	f"   {Fore.WHITE}{Style.DIM}(using train tokens as val tokens)" if CONFIG["train_data"] == CONFIG["val_data"] else "", filename=model_log_path
+	f"   {Fore.WHITE}{Style.DIM}(using train tokens as val tokens)" if CONFIG["train_data"] == CONFIG["val_data"] else ""
 )
 del num_train_toks, num_val_toks
 
 # report number of parameters
-kprint(f"{Fore.WHITE}{Style.BRIGHT}{model.get_num_params()/1e6}M", "parameters", filename=model_log_path)
+print0(f"{Fore.WHITE}{Style.BRIGHT}{sum(p.numel() for p in model.parameters())/1e6}M", "parameters")
 
 # initialize a GradScaler. If enabled=False scaler is a no-op
 scaler = torch.amp.GradScaler(enabled=False)
 
 # compile the model
 if CONFIG["compile"]:
-	kprint(f"compiling the model... {Fore.WHITE}{Style.DIM}(takes a ~minute)", filename=model_log_path)
-	model = torch.compile(model, mode="max-autotune") # requires PyTorch 2.0
+	print0(f"compiling the model... {Fore.WHITE}{Style.DIM}(takes a ~minute)")
+	model = torch.compile(model) # requires PyTorch 2.0
 
 # training loop
 X, Y = train_data_loader.next_batch() # fetch the very first batch
@@ -221,8 +231,7 @@ eval_t0 = time.time()
 t0 = time.time()
 t2 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
-running_mfu = -1.0 if stats["mfu"] == [] else stats["mfu"][-1]
-training_loop = True
+running_mfu = -1.0
 
 # load encoder and sample constructor
 # for sampling text using training
@@ -239,156 +248,125 @@ def get_trained_model(model, optimizer):
 		"stats": stats
 	}
 
-while training_loop:
-	try:
-		# determine and set the learning rate for this iteration
-		lr = get_lr(stats["iter_num"]) if CONFIG["decay_lr"] else CONFIG["learning_rate"]
-		for param_group in optimizer.param_groups:
-			param_group["lr"] = lr
-		stats["lr"].append(lr)
+# write checkpoints
+def save_checkpoint():
+	if CONFIG["checkpoints"] == None or stats["steps"] <= 0 or stats["steps"] % CONFIG["checkpoints"]["interval"] != 0: return
+	if not os.path.isdir(CONFIG["checkpoints"]["path"]): os.mkdir(CONFIG["checkpoints"]["path"])
+	print0(f"saved checkpoint at step {Fore.WHITE}{Style.BRIGHT}{stats["steps"]}")
+	torch.save(get_trained_model(model, optimizer), f"{CONFIG["checkpoints"]["path"]}/s{stats["steps"]}.bin")
 
-		# save checkpoint
-		if CONFIG["checkpoints"] != None and stats["iter_num"] > 0 and stats["iter_num"] % CONFIG["checkpoints"]["interval"] == 0:
-			if not os.path.isdir(CONFIG["checkpoints"]["path"]):
-				os.mkdir(CONFIG["checkpoints"]["path"])
+# generate some sample text
+def sample_output():
+	if CONFIG["sample_interval"] == None or stats["steps"] <= 0 or stats["steps"] % CONFIG["sample_interval"] != 0: return
+	training_sample.load(get_trained_model(model, optimizer), True)
+	print0(f"{Fore.WHITE}{Style.DIM}```s{stats["steps"]}.bin\n{enc.decode(training_sample.generate(None, length=CONFIG["block_size"]))}\n```")
 
-			kprint(f"saved checkpoint at step {Fore.WHITE}{Style.BRIGHT}{stats["iter_num"]}", filename=model_log_path)
-			torch.save(get_trained_model(model, optimizer), f"{CONFIG["checkpoints"]["path"]}/s{stats["iter_num"]}.bin")
+# evaluate the loss on train/val sets
+def log_eval_loss():
+	if stats["steps"] <= 0 or stats["steps"] % CONFIG["eval_interval"] != 0: return
+	global eval_t0
+	# timing and logging
+	losses = estimate_loss(CONFIG["eval_iters"], model, get_batch)
+	eval_t1 = time.time()
+	eval_dt = eval_t1 - eval_t0
+	eval_t0 = eval_t1
+	print0(
+		f"{Fore.WHITE}{Style.BRIGHT}step",
+		f"{Fore.WHITE}{Style.DIM}[{stats["steps"]}/{CONFIG["max_iters"]}]"
+		f"{Fore.RESET}{Style.RESET_ALL}:",
+		f"train loss {Fore.WHITE}{Style.BRIGHT}{losses["train"]:.4f}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"val loss {Fore.WHITE}{Style.BRIGHT}{losses["val"]:.4f}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"lr {Fore.WHITE}{Style.BRIGHT}{lr:.7f}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"time took {Fore.WHITE}{Style.DIM}{calc_total_time(eval_dt)}"
+	)
+	stats["train"].append(losses["train"])
+	stats["val"].append(losses["val"])
 
-		# generate some sample text
-		if CONFIG["sample_interval"] != None and stats["iter_num"] > 0 and stats["iter_num"] % CONFIG["sample_interval"] == 0:
-			training_sample.load(get_trained_model(model, optimizer), True)
+def log_loss():
+	global local_iter_num, running_mfu, t0, t2
+	# timing and logging
+	t1 = time.time()
+	dt = t1 - t0
+	t0 = t1
 
-			for _ in range(CONFIG["sample_iters"]):
-				out = enc.decode(training_sample.generate(None, length=CONFIG["block_size"]))
-				kprint(f"{Fore.WHITE}{Style.DIM}```s{stats["iter_num"]}.bin\n{out}\n```", filename=model_log_path)
+	if stats["steps"] % CONFIG["log_interval"] != 0:
+		return
 
-		# evaluate the loss on train/val sets and write checkpoints
-		if stats["iter_num"] > 0 and stats["iter_num"] % CONFIG["eval_interval"] == 0:
-			losses = estimate_loss(CONFIG["eval_iters"], model, get_batch)
-			# timing and logging
-			eval_t1 = time.time()
-			eval_dt = eval_t1 - eval_t0
-			eval_t0 = eval_t1
+	# timing and logging
+	t1 = time.time()
+	dt2 = t1 - t2
+	t2 = t1
 
-			kprint(
-				f"{Fore.WHITE}{Style.BRIGHT}step",
-				f"{Fore.WHITE}{Style.DIM}[{stats["iter_num"]}/{CONFIG["max_iters"]}]"
-				f"{Fore.RESET}{Style.RESET_ALL}:",
-				f"train loss {Fore.WHITE}{Style.BRIGHT}{losses["train"]:.4f}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"val loss {Fore.WHITE}{Style.BRIGHT}{losses["val"]:.4f}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"lr {Fore.WHITE}{Style.BRIGHT}{lr:.7f}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"time took {Fore.WHITE}{Style.DIM}{calc_total_time(eval_dt)}",
-				filename=model_log_path
-			)
+	# get loss as float. note: this is a CPU-GPU sync point
+	# scale up to undo the division above, approximating the true total loss (exact would have been a sum)
+	lossf = loss.item() * CONFIG["gradient_accumulation_steps"]
 
-			stats["train"].append(losses["train"])
-			stats["val"].append(losses["val"])
+	if local_iter_num >= 5: # let the training loop settle a bit
+		mfu = model.estimate_mfu(CONFIG["batch_size"] * CONFIG["gradient_accumulation_steps"] * CONFIG["log_interval"], dt) # https://github.com/karpathy/nanoGPT/pull/527/files
+		running_mfu = mfu if running_mfu == -1.0 else 0.9 * running_mfu + 0.1 * mfu
 
-		# forward backward update, with optional gradient accumulation to simulate larger batch size
-		# and using the GradScaler if data type is float16
-		for micro_step in range(CONFIG["gradient_accumulation_steps"]):
-			with ctx:
-				logits, loss = model(X, Y)
-				loss = loss / CONFIG["gradient_accumulation_steps"] # scale the loss to account for gradient accumulation
+	toks_per_sec = (CONFIG["batch_size"] * CONFIG["gradient_accumulation_steps"] * CONFIG["block_size"]) / dt
+	print0(
+		f"{Fore.WHITE}{Style.BRIGHT}iter",
+		f"{Fore.WHITE}{Style.DIM}[{stats["steps"]}/{CONFIG["max_iters"]}]"
+		f"{Fore.RESET}{Style.RESET_ALL}:",
+		f"loss {Fore.WHITE}{Style.BRIGHT}{lossf:.4f}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"mfu {Fore.WHITE}{Style.BRIGHT}{running_mfu*100:.2f}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"dt {Fore.WHITE}{Style.DIM}{calc_total_time(dt2)}"
+		f"{Fore.RESET}{Style.RESET_ALL},",
+		f"tok/s {Fore.WHITE}{Style.DIM}{toks_per_sec:.2f}"
+	)
+	stats["eval"].append(lossf)
 
-			# immediately async prefetch next batch while model is doing the forward pass on the GPU
-			X, Y = train_data_loader.next_batch()
-			# backward pass, with gradient scaling if training in fp16
-			scaler.scale(loss).backward()
+while True:
+	# determine and set the learning rate for this iteration
+	lr = get_lr(stats["steps"]) if CONFIG["decay_lr"] else CONFIG["learning_rate"]
+	for group in optimizer.param_groups:
+		group["lr"] = lr
+	stats["lr"].append(lr)
 
-		# clip the gradient
-		if CONFIG["grad_clip"] != 0.0:
-			scaler.unscale_(optimizer)
-			torch.nn.utils.clip_grad_norm_(model.parameters(), CONFIG["grad_clip"])
+	# save checkpoint and log sample and eval loss
+	save_checkpoint()
+	sample_output()
+	log_eval_loss()
 
-		# step the optimizer and scaler if training in fp16
-		scaler.step(optimizer)
-		scaler.update()
+	# forward backward update, with optional gradient accumulation to simulate larger batch size
+	# and using the GradScaler if data type is float16
+	for micro_step in range(CONFIG["gradient_accumulation_steps"]):
+		with ctx:
+			logits, loss = model(X, Y)
+			loss = loss / CONFIG["gradient_accumulation_steps"] # scale the loss to account for gradient accumulation
 
-		# flush the gradients as soon as we can, no need for this memory anymore
-		optimizer.zero_grad(set_to_none=True)
+		# immediately async prefetch next batch while model is doing the forward pass on the GPU
+		X, Y = train_data_loader.next_batch()
+		# backward pass, with gradient scaling if training in fp16
+		scaler.scale(loss).backward()
 
-		# timing and logging
-		t1 = time.time()
-		dt = t1 - t0
-		t0 = t1
-		if stats["iter_num"] % CONFIG["log_interval"] == 0:
-			t1 = time.time()
-			dt2 = t1 - t2
-			t2 = t1
+	# clip the gradient
+	if CONFIG["grad_clip"] != 0.0:
+		scaler.unscale_(optimizer)
+		torch.nn.utils.clip_grad_norm_(model.parameters(), CONFIG["grad_clip"])
 
-			# get loss as float. note: this is a CPU-GPU sync point
-			# scale up to undo the division above, approximating the true total loss (exact would have been a sum)
-			lossf = loss.item() * CONFIG["gradient_accumulation_steps"]
+	# step the optimizer and scaler if training in fp16
+	scaler.step(optimizer)
+	scaler.update()
 
-			if local_iter_num >= 5: # let the training loop settle a bit
-				mfu = model.estimate_mfu(CONFIG["batch_size"] * CONFIG["gradient_accumulation_steps"] * CONFIG["log_interval"], dt) # https://github.com/karpathy/nanoGPT/pull/527/files
-				running_mfu = mfu if running_mfu == -1.0 else 0.9 * running_mfu + 0.1 * mfu
+	# flush the gradients as soon as we can, no need for this memory anymore
+	optimizer.zero_grad(set_to_none=True)
 
-			toks_per_sec = (CONFIG["batch_size"] * CONFIG["gradient_accumulation_steps"] * CONFIG["block_size"]) / dt
-			kprint(
-				f"{Fore.WHITE}{Style.BRIGHT}iter",
-				f"{Fore.WHITE}{Style.DIM}[{stats["iter_num"]}/{CONFIG["max_iters"]}]"
-				f"{Fore.RESET}{Style.RESET_ALL}:",
-				f"loss {Fore.WHITE}{Style.BRIGHT}{lossf:.4f}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"mfu {Fore.WHITE}{Style.BRIGHT}{running_mfu*100:.2f}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"dt {Fore.WHITE}{Style.DIM}{calc_total_time(dt2)}"
-				f"{Fore.RESET}{Style.RESET_ALL},",
-				f"tok/s {Fore.WHITE}{Style.DIM}{toks_per_sec:.2f}",
-				filename=model_log_path
-			)
-			stats["mfu"].append(running_mfu)
-			stats["eval"].append(lossf)
+	# log loss
+	log_loss()
+	stats["steps"] += 1
+	local_iter_num += 1
 
-		stats["iter_num"] += 1
-		local_iter_num += 1
+	# termination conditions
+	if stats["steps"] > CONFIG["max_iters"]:
+		break
 
-		# termination conditions
-		if stats["iter_num"] > CONFIG["max_iters"]:
-			break
-
-	except KeyboardInterrupt:
-		print("type")
-		print(f"{Fore.WHITE}{Style.BRIGHT}1. {Fore.WHITE}{Style.DIM}`y` {Style.RESET_ALL}to stop training.")
-		print(f"{Fore.WHITE}{Style.BRIGHT}2. {Fore.WHITE}{Style.DIM}`n` {Style.RESET_ALL}to continue training.")
-		print(f"{Fore.WHITE}{Style.BRIGHT}3. {Fore.WHITE}{Style.DIM}`s` {Style.RESET_ALL}to save model.")
-		print(f"{Fore.WHITE}{Style.BRIGHT}4. {Fore.WHITE}{Style.DIM}`r` {Style.RESET_ALL}to reload config.json.")
-		print(f"{Fore.WHITE}{Style.BRIGHT}5. {Fore.WHITE}{Style.DIM}`e` {Style.RESET_ALL}to force exit without save.")
-
-		while True:
-			inp = input("> ")
-
-			if inp == "y":
-				print(f"{Fore.RED}{Style.BRIGHT}early stopping.")
-				training_loop = False
-				break
-
-			elif inp == "n":
-				print(f"{Fore.GREEN}{Style.BRIGHT}continue training.")
-				break
-
-			elif inp == "s":
-				print(f"{Fore.YELLOW}{Style.BRIGHT}saving model.")
-				print("total time:", calc_total_time(time.time() - start_time))
-				torch.save(get_trained_model(model, optimizer), CONFIG["save_path"])
-
-			elif inp == "r":
-				print(f"{Fore.YELLOW}{Style.BRIGHT}config.json{Style.RESET_ALL} reloaded.")
-				with open(CONFIG_PATH, "r", encoding="utf-8") as f:
-					CONFIG = json.load(f)
-
-			if inp == "e":
-				print(f"{Fore.RED}{Style.BRIGHT}forcing exit without save.")
-				sys.exit()
-
-			else:
-				print(f"{Fore.RED}{Style.DIM}Wrong option.")
-
-kprint("total time:", calc_total_time(time.time() - start_time), filename=model_log_path)
+print0("total time:", calc_total_time(time.time() - start_time))
 torch.save(get_trained_model(model, optimizer), CONFIG["save_path"])
